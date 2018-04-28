a36ee2192704a78e40aa9201caedb153bb2d43fe
fizzX <william.drew100@gmail.com>
2018-04-27 20:29:21 -0400

    got string stream for parser, working on getting token stream

--- parser.ml
+++ parser.ml
@@ -1,53 +1,64 @@
 open Scanf;;
+open NativeLazyStreams;;
+open String;;
 
-type reader = {read_next : unit -> string option};;
-
-let make_reader file_name =
-  let in_channel = open_in file_name in
-  let closed = ref false in
-  let input = Scanning.from_channel in_channel in
-  let read_next_line = fun () ->
-    if !closed then
-      None
-    else
-      try
-        Some (Scanf.bscanf input "%[^\r\n]\n" (fun x -> x))
-      with
-        End_of_file ->
-          let _ = close_in_noerr in_channel in
-          let _ = closed := true in
-          None in
-  {read_next = read_next_line};;
-
-let r = make_reader "test.txt";;
-let next_line = r.read_next ();;
-
-
+(* input token types : words, punctuation, end of sentence, and end of file *)
 type token =
   | End
   | Word of string
   | Punct of string
+  | Eof
+
+class type parser_t =
+object
+  method tokenize : string -> string list
+  method get_string : unit -> string
+  method input_stream : unit -> string Stream.t
+  method token_stream : unit -> token Stream.t
+end ;;
 
 
+class parser (s : string) : parser_t =
+object(this)
 
+  val mutable input_list : string list ref = ref []
 
+  val mutable lines : string Stream.t =
+    let in_channel = open_in s in
+    Stream.from (fun _ -> try Some (input_line in_channel) with End_of_file -> None)
 
+  method tokenize (line : string) : string list = String.split_on_char ' ' line
 
+  method get_string () : string =
+    match !input_list with
+    | [] -> input_list := tokenize (Stream.next lines); this#get_string()
+    | hd :: tl -> input_list := tl; hd
 
+  method input_stream () : string Stream.t =
+    Stream.from (fun _ -> Some (this#get_string()))
 
+  method stream_map f stream =
+    let rec next i =
+      try Some (f (Stream.next stream))
+      with Stream.Failure -> None in
+    Stream.from next
 
+  method token_of_string (s : string) : token =
+    match s with
+    |
+    |
 
+  method token_stream () : token Stream.t =
+    stream_map (fun x -> ) input_stream()
 
+end;;
 
+let test = new parser "test.txt";;
+let stream = test#input_stream();;
 
+open Str ;;
 
-
-
-
-
-
-
-
+let tokenize (line : string) : string list = Str.split (regexp "[ \t]+") (Str.global_replace (regexp "[[:punct:]]") " " line);;
 
 
 
