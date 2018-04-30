abba1c5eabf370df5caf9ae180d0abab2fb16be7
imuhammad21 <31607958+imuhammad21@users.noreply.github.com>
2018-04-30 15:46:06 -0400

    Update generator.ml

--- generator.ml
+++ generator.ml
@@ -6,16 +6,18 @@ open Parser ;;
 open Markov ;;
 
 module type GENERATOR =
-  sig 
-    val roll : mchain -> string -> string option
-    val gen : mchain -> token -> token list
-  end
+    sig 
+    	val roll : mchain -> string -> string option
+		val gen : mchain -> token -> string
+	end
 
 module Generator : GENERATOR = 
-  struct
-    let roll = MarkovChain.roll
-    let rec gen (m: mchain) (word: token) : token list  =
-      match roll m word with
-      |Some w -> w :: gen m w
-      |None -> []
-  end
+	struct
+		let roll = Markovchain.roll
+		let rec gen (m: mchain) (word: token) : string  =
+			match roll m word with
+			|Some w -> match w with 
+					   |End -> ""
+					   |_ -> w ^ " " ^ gen m w
+			|None -> []
+	end
