f6a8f60a121b7e4e632da251266e053d7a5c54a9
fizzX <william.drew100@gmail.com>
2018-04-30 16:44:31 -0400

    added tab consideration

--- parser.ml
+++ parser.ml
@@ -20,7 +20,7 @@ module Parser : PARSER =
 
     let tokenize (s : string) : string list =
       let p = Str.regexp "\\([\\.!?,]+\\)" in
-      Str.split (regexp " +") (Str.global_replace p " \\1 " s)
+      Str.split (regexp "\s+") (Str.global_replace p " \\1 " s)
 
     let input_stream (s : string Stream.t) : token Stream.t =
       let l = ref None in
