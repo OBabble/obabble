b3510a51a3730d5078f89ab0622d3244c1e94bd1
George Moe <geomo3@gmail.com>
2018-05-01 15:22:31 -0400

    Demoduled things.

--- generator.ml
+++ generator.ml
@@ -6,32 +6,21 @@ open Parser ;;
 open Markov ;;
 open Token ;;
 
-module type GENERATOR =
-  sig
-    exception WordNotFound of token
-    val reps : int
-    val gen : mchain -> token -> token list
-    val repeat: mchain -> token
-  end
+exception WordNotFound of token
 
-module Generator : GENERATOR =
-  struct
-    exception WordNotFound of token
-    
-    let roll = MarkovChain.roll
-    
-    let rec gen (m: mchain) (word: token) : token list  =
-      match roll m word with
-      |Some w -> (match w with 
-            |End -> []
-            |s -> s :: gen m w)
-      |None -> raise (WordNotFound (word))
-      
-    let scorer (m : mchain) (query : token list) (answer : token list) : float =
-      let string_list = List.map token_to_string l in
-      
-    let rec repeat (m: mchain) (word: token) : token list list =  
-    	let reps = reps + 1 in
-          if reps <= 10 then gen m word :: repeat m word
-	  else gen m word :: []
-  end
+let roll = MarkovChain.roll
+
+let rec gen (m: mchain) (word: token) : token list  =
+  match roll m word with
+  |Some w -> (match w with 
+        | End -> []
+        | Word s -> s :: gen m w)
+  |None -> raise (WordNotFound (word))
+  
+let scorer (m : mchain) (query : token list) (answer : token list) : float =
+  let string_list = List.map token_to_string l in
+  
+let rec repeat (m: mchain) (word: token) : token list list =  
+  let reps = reps + 1 in
+      if reps <= 10 then gen m word :: repeat m word
+else gen m word :: []
--- parser.ml
+++ parser.ml
@@ -3,36 +3,25 @@ open String;;
 open Str;;
 open Token ;;
 
-module type PARSER =
-  sig
-    val get_stream : string -> token Stream.t
-    val get_user_token_list : string -> token list
-  end ;;
+let input_stream (s : string Stream.t) : token Stream.t =
+  let l = ref None in
+  let rec get_string (s : string Stream.t) : token =
+    (match !l with
+     | None -> l := Some (tokenize (Stream.next s));
+                    get_string s
+     | Some [] -> l:= None; End
+     | Some (hd :: tl) -> l := Some tl; Word hd)
+  in
+  Stream.from (fun _ -> Some (get_string s))
 
-module Parser : PARSER =
-  struct
+let get_stream (s : string) : token Stream.t =
+  let in_channel = open_in s in
+  let string_stream = Stream.from (fun _ -> try
+                                              Some (input_line in_channel)
+                                            with
+                                              End_of_file -> None)
+  in
+  input_stream string_stream
 
-    let input_stream (s : string Stream.t) : token Stream.t =
-      let l = ref None in
-      let rec get_string (s : string Stream.t) : token =
-        (match !l with
-         | None -> l := Some (tokenize (Stream.next s));
-                        get_string s
-         | Some [] -> l:= None; End
-         | Some (hd :: tl) -> l := Some tl; Word hd)
-      in
-      Stream.from (fun _ -> Some (get_string s))
-
-    let get_stream (s : string) : token Stream.t =
-      let in_channel = open_in s in
-      let string_stream = Stream.from (fun _ -> try
-                                                  Some (input_line in_channel)
-                                                with
-                                                  End_of_file -> None)
-      in
-      input_stream string_stream
-
-    let get_user_token_list (s : string) : token list =
-      List.map (fun x -> Word x) (tokenize s)
-
-  end ;;
+let get_user_token_list (s : string) : token list =
+  List.map (fun x -> Word x) (tokenize s)
