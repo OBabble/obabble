4d3ca73385f4bd3109c6747c2adf55b1dac04362
imuhammad21 <31607958+imuhammad21@users.noreply.github.com>
2018-05-01 14:42:01 -0400

    Update generator.ml

--- generator.ml
+++ generator.ml
@@ -16,14 +16,17 @@ module Generator : GENERATOR =
   struct
     exception WordNotFound of token
     let roll = MarkovChain.roll
-    let rec gen (m: mchain) (word: token) : string  =
+    let rec gen (m: mchain) (word: token) : token list  =
       match roll m word with
       |Some w -> (match w with
-            |End -> ""
-            |Word s -> s ^ " " ^ gen m w)
+            |End -> []
+            |s -> s :: gen m w)
       |None -> raise (WordNotFound (word))
+      
     let scorer (m : mchain) (query : token list) (answer : token list) : float =
       let string_list = List.map token_to_string l in
-
-
+    let rec repeat (m: mchain) (word: token) : token list list =  
+    	let i = i + 1 in
+        if i <= 10 then gen m word :: repeat m word
+	      else gen m word :: []
   end
