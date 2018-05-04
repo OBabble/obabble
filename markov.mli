33b9c421a0362b899a1f1c4ccb3122ff728f275b
George Moe <geomo3@gmail.com>
2018-05-04 14:36:06 -0400

    Rewrote roll to give absolute result, in O(logn) time.

--- markov.mli
+++ markov.mli
@@ -12,7 +12,7 @@ module type MARKOVCHAIN =
     val token_list : mchain -> token -> token list
     val token_totals : mchain -> token -> int
     val query : mchain -> token -> token -> (int * int) option
-    val roll : int -> mchain -> token -> token option
+    val roll : mchain -> token -> token option
     val size : mchain -> int * int
     val load : string -> mchain
     val save : mchain -> string -> unit
